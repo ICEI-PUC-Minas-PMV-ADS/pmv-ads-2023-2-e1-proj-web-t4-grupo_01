0c64948585a97e699a9f2361fb129dc4176e2b95
Pedro de Oliveira Rosas <8bitsspike@gmail.com>
2023-10-17 08:21:33 -0300

    Página modelo e script.js

---
+++ codigo-fonte/script.js
@@ -0,0 +1,17 @@
+/* O codigo neste documento deve estar organizado em sessões, o topo do documento deve conter
+todo script que pode(e vai) ser usado por mais de uma página, as sessões seguintes devem conter
+a seguinte organização:
+ -Código da página "Nome da pagina" (endereço)-
+ Script usado no header
+ Script usado no body
+ Script unicos da página que não dependem de css ou HTML 
+ */
+
+ /* -Código de uso em mais de uma página- */
+
+ /* -Código da página login (index.html)- */
+
+ /* -Código da pagina formulário de usuário vonluntário- */
+
+ /* -Código da pagina formulário de usuário organizacional- */
+ 
