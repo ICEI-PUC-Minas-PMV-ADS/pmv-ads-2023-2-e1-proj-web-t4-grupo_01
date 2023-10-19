8f8529ac94dbaead75118b5b0838c31c1accf653
Pedro de Oliveira Rosas <8bitsspike@gmail.com>
2023-10-19 20:35:34 -0300

    Atualizei o Style.css

--- codigo-fonte/script.js
+++ codigo-fonte/script.js
@@ -1,6 +1,7 @@
 /* O codigo neste documento deve estar organizado em sessões, o topo do documento deve conter
 todo script que pode(e vai) ser usado por mais de uma página, as sessões seguintes devem conter
 a seguinte organização:
+
  -Código da página "Nome da pagina" (endereço)-
  Script usado no header
  Script usado no body
