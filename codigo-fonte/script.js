71b98a14fef27c734a017048bd0204f3fcdd12b7
Leticia Mateus <145443338+Let788@users.noreply.github.com>
2023-10-27 00:31:22 +0100

    Pagina Formulario de Usuario inicio

--- codigo-fonte/script.js
+++ codigo-fonte/script.js
@@ -15,4 +15,12 @@ a seguinte organização:
  /* -Código da pagina formulário de usuário vonluntário- */
 
  /* -Código da pagina formulário de usuário organizacional- */
+
+ /* mudar nome do imput da foto para formulario de informações*/
+ document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("btn").addEventListener("click",function() { 
+        document.getElementById("myfile").click();
+      }, false);
+  });
  
+  
